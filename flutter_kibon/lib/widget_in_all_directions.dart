df3413cf3dbec8f4a24fb3ef815ac75c0f412b2d
YoonSung Kim <opqudk@gmail.com>
2024-10-21 21:12:12 +0900

    🖊️ Widget in all directions

--- flutter_kibon/lib/widget_in_all_directions.dart
+++ flutter_kibon/lib/widget_in_all_directions.dart
@@ -8,34 +8,50 @@ class WidgetInAlldirections extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('위젯을 상하좌우로 배치')),
-        body: Container(
-          width: double.infinity,
-          height: 300,
-          color: Colors.grey,
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 100,
-                height: 80,
-                color: Colors.red,
-                child: const Text('Container1'),
-              ),
-              Container(
-                width: 100,
-                height: 80,
-                color: Colors.green,
-                child: const Text('Container2'),
-              ),
-              Container(
-                width: 100,
-                height: 80,
-                color: Colors.blue,
-                child: const Text('Container2'),
-              ),
-            ],
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            height: 1300,
+            color: Colors.grey,
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 300,
+                        height: 80,
+                        color: Colors.red,
+                        child: const Text('Container1'),
+                      ),
+                      const SizedBox(width: 20),
+                      Container(
+                        width: 300,
+                        height: 80,
+                        color: Colors.green,
+                        child: const Text('Container2'),
+                      ),
+                      Container(
+                        width: 300,
+                        height: 80,
+                        color: Colors.blue,
+                        child: const Text('Container2'),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.blueGrey,
+                  width: 300,
+                  height: 1000,
+                  child: const Text('Container 4'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
