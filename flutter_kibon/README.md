08929bbf82d012537bfbcbc92a724189de4fc15b
YoonSung Kim <opqudk@gmail.com>
2024-10-21 11:03:40 +0900

    🎉 flutter init

---
+++ flutter_kibon/README.md
@@ -0,0 +1,3 @@
+# flutter_kibon
+
+A new Flutter project.
---
+++ flutter_kibon/android/app/src/main/kotlin/com/example/flutter_kibon/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_kibon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
