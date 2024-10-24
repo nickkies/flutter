33f4eafe9f05d87358e63b2c245712019de0987c
YoonSung Kim <opqudk@gmail.com>
2024-10-24 13:45:17 +0900

    💄 design: user input

--- rps_game/lib/game/user_input.dart
+++ rps_game/lib/game/user_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rps_game/game/enum.dart';
 
 class UserInput extends StatelessWidget {
   final bool isDone;
@@ -7,6 +8,28 @@ class UserInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('UserInput');
+    if (isDone) {
+      return const Placeholder();
+    }
+
+    return Row(children: _getInputs());
+  }
+
+  List<Widget> _getInputs() {
+    return InputType.values
+        .map(
+          (e) => Expanded(
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24),
+                border: Border.all(color: Colors.grey, width: 8),
+              ),
+              child: Image.asset(e.path),
+            ),
+          ),
+        )
+        .toList();
   }
 }
