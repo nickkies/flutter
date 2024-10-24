3f18afa225e9b6ef9b99d2b4cce22ac3c31f5c58
YoonSung Kim <opqudk@gmail.com>
2024-10-24 16:10:43 +0900

    💄 design: cpu input

--- rps_game/lib/game/widget/cpu_input.dart
+++ rps_game/lib/game/widget/cpu_input.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:rps_game/game/enum.dart';
+import 'package:rps_game/game/widget/input_card.dart';
 
 class CpuInput extends StatelessWidget {
   final bool isDone;
@@ -7,6 +9,28 @@ class CpuInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('CpuInput');
+    return Row(
+      children: [
+        const Expanded(child: SizedBox.shrink()),
+        Expanded(child: InputCard(child: _getCpuInput())),
+        const Expanded(child: SizedBox.shrink()),
+      ],
+    );
+  }
+
+  Widget _getCpuInput() {
+    if (isDone) {
+      return Image.asset(InputType.rock.path);
+    }
+
+    return const SizedBox(
+      height: 80,
+      child: Center(
+        child: Text(
+          '?',
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
