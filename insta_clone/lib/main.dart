0221ca6db380ba29380d858d13c813e1a4c07a7e
YoonSung Kim <opqudk@gmail.com>
2024-10-25 15:21:50 +0900

    💄design: Home

--- insta_clone/lib/main.dart
+++ insta_clone/lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:insta_clone/body.dart';
 
 void main() {
   runApp(const InstaCloneApp());
@@ -9,6 +13,67 @@ class InstaCloneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Colors.white,
+          secondary: Colors.black,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: Colors.black,
+        ),
+      ),
+      home: const InstaCloneHome(),
+    );
+  }
+}
+
+class InstaCloneHome extends StatefulWidget {
+  const InstaCloneHome({super.key});
+
+  @override
+  State<InstaCloneHome> createState() => _InstaCloneHomeState();
+}
+
+class _InstaCloneHomeState extends State<InstaCloneHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Instagram',
+          style: GoogleFonts.lobsterTwo(color: Colors.black, fontSize: 32),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.favorite_outline,
+              size: 32,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.paperplane, size: 32),
+          ),
+        ],
+      ),
+      body: const InstaBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 28),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, size: 28),
+            label: 'Search',
+          )
+        ],
+      ),
+    );
   }
 }
