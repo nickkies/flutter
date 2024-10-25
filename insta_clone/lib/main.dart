505ead8fb2f2907488385ed6b65fd4a93215a376
YoonSung Kim <opqudk@gmail.com>
2024-10-25 14:03:57 +0900

    🎉 insta clone init

---
+++ insta_clone/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const InstaCloneApp());
+}
+
+class InstaCloneApp extends StatelessWidget {
+  const InstaCloneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
