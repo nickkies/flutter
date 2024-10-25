10950d2b0357fda930b1b2eeef8bce0a31021ac4
YoonSung Kim <opqudk@gmail.com>
2024-10-25 10:19:26 +0900

    💄design: game result

--- rps_game/lib/game/widget/game_result.dart
+++ rps_game/lib/game/widget/game_result.dart
@@ -7,6 +7,24 @@ class GameResult extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('GameResult');
+    if (isDone) {
+      return Column(
+        children: [
+          const Text('승리!', style: TextStyle(fontSize: 32)),
+          const SizedBox(height: 10),
+          TextButton(
+            onPressed: () {},
+            child: const Text('다시하기', style: TextStyle(fontSize: 18)),
+          )
+        ],
+      );
+    }
+
+    return const Center(
+      child: Text(
+        '가위~ 바위~ 보!',
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      ),
+    );
   }
 }
