8b55bc9a0abd487716075a248a0661ccc1e08906
YoonSung Kim <opqudk@gmail.com>
2024-10-25 17:12:07 +0900

    💄design: Story Area

--- insta_clone/lib/screen/home_screen.dart
+++ insta_clone/lib/screen/home_screen.dart
@@ -5,6 +5,50 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('home');
+    return const SingleChildScrollView(
+      child: Column(children: [StoryArea(), FeedList()]),
+    );
+  }
+}
+
+class StoryArea extends StatelessWidget {
+  const StoryArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(
+          10,
+          (i) => Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+            child: Column(
+              children: [
+                Container(
+                  width: 80,
+                  height: 80,
+                  margin: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade300,
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                ),
+                Text('User $i'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FeedList extends StatelessWidget {
+  const FeedList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
