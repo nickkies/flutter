33bfb242262b195d9e9fa1d512906dc582ead9f2
YoonSung Kim <opqudk@gmail.com>
2024-10-26 23:41:27 +0900

    💄 design: Search text bar

--- insta_clone/lib/screen/search_screen.dart
+++ insta_clone/lib/screen/search_screen.dart
@@ -5,6 +5,53 @@ class SearchScreeen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('search');
+    return const SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [SearchTextBar(), SearchGrid()],
+        ),
+      ),
+    );
+  }
+}
+
+class SearchTextBar extends StatelessWidget {
+  const SearchTextBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: TextField(
+          cursorColor: Colors.black87,
+          decoration: InputDecoration(
+            prefixIcon: const Icon(Icons.search),
+            hintText: '검색',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.grey.shade200, width: 1),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.grey.shade100, width: 1),
+            ),
+            filled: true,
+            fillColor: Colors.grey.shade200,
+            contentPadding: const EdgeInsets.all(0),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchGrid extends StatelessWidget {
+  const SearchGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
