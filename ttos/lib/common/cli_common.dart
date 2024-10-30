6d20d37c38e8da5a2d27d48bbdfd237584a5ec82
YoonSung Kim <opqudk@gmail.com>
2024-10-30 22:06:40 +0900

    🎉 init from template

---
+++ ttos/lib/common/cli_common.dart
@@ -0,0 +1,3 @@
+export 'package:ttos/common/dart/extension/collection_extension.dart';
+export 'package:ttos/common/dart/extension/num_duration_extension.dart';
+export 'package:ttos/common/util/async/cli_async.dart';
---
+++ ttos/lib/common/data/preference/prefs.dart
@@ -0,0 +1,6 @@
+import 'package:ttos/common/data/preference/item/nullable_preference_item.dart';
+import 'package:ttos/common/theme/custom_theme.dart';
+
+class Prefs {
+  static final appTheme = NullablePreferenceItem<CustomTheme>('appTheme');
+}
