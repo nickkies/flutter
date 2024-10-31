a3328c836ae95dff52ab8f206a117fd1ec65ea5e
YoonSung Kim <opqudk@gmail.com>
2024-10-31 15:46:28 +0900

    ✨ feat: Add tabs

---
+++ ttos/lib/screen/main/tab/ttos_pay/f_ttos_pay.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TtosPayFragment extends StatefulWidget {
+  const TtosPayFragment({super.key});
+
+  @override
+  State<TtosPayFragment> createState() => _TtosPayFragmentState();
+}
+
+class _TtosPayFragmentState extends State<TtosPayFragment> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('또스페이');
+  }
+}
