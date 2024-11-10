58357191a42c9d5f92bc163dea59f17cb1f57c93
YoonSung Kim <opqudk@gmail.com>
2024-11-10 17:16:47 +0900

    ♻️ refactor: Use getx

--- ttos/lib/common/data/preference/prefs.dart
+++ ttos/lib/common/data/preference/prefs.dart
@@ -1,8 +1,8 @@
 import 'package:ttos/common/data/preference/item/nullable_preference_item.dart';
-import 'package:ttos/common/data/preference/item/preference_item.dart';
+import 'package:ttos/common/data/preference/item/rx_preference_item.dart';
 import 'package:ttos/common/theme/custom_theme.dart';
 
 class Prefs {
   static final appTheme = NullablePreferenceItem<CustomTheme>('appTheme');
-  static final isPushOn = PreferenceItem<bool>('isPushOn', false);
+  static final isPushOnRx = RxPreferenceItem<bool, RxBool>('isPushOn', false);
 }
--- ttos/lib/screen/main/tab/stock/setting/s_setting.dart
+++ ttos/lib/screen/main/tab/stock/setting/s_setting.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ttos/common/common.dart';
 import 'package:ttos/common/data/preference/prefs.dart';
 import 'package:ttos/screen/main/tab/stock/setting/w_switch_menu.dart';
@@ -11,27 +12,18 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
-  late bool isPushOn;
-
-  @override
-  void initState() {
-    isPushOn = Prefs.isPushOn.get();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: '설정'.text.make()),
       body: ListView(
         children: [
-          SwitchMenuWidget(
-            '푸시 설정',
-            isPushOn,
-            onTap: (isOn) {
-              Prefs.isPushOn.set(isOn);
-              setState(() => isPushOn = isOn);
-            },
+          Obx(
+            () => SwitchMenuWidget(
+              '푸시 설정',
+              Prefs.isPushOnRx.get(),
+              onTap: (isOn) => Prefs.isPushOnRx.set(isOn),
+            ),
           ),
 
           // slider
