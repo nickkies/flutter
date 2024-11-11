d3b03bf9f377170f671979c94c23eb4b97678b36
YoonSung Kim <opqudk@gmail.com>
2024-11-11 15:29:52 +0900

    ✨ feat: Save DateTime

--- ttos/lib/common/data/preference/prefs.dart
+++ ttos/lib/common/data/preference/prefs.dart
@@ -1,5 +1,6 @@
 import 'package:ttos/common/data/preference/item/nullable_preference_item.dart';
 import 'package:ttos/common/data/preference/item/rx_preference_item.dart';
+import 'package:ttos/common/data/preference/item/rxn_preference_item.dart';
 import 'package:ttos/common/theme/custom_theme.dart';
 
 class Prefs {
@@ -7,4 +8,6 @@ class Prefs {
   static final isPushOnRx = RxPreferenceItem<bool, RxBool>('isPushOn', false);
   static final sliderPosiion =
       RxPreferenceItem<double, RxDouble>('sliderPosition', 0.0);
+  static final birthday =
+      RxnPreferenceItem<DateTime, Rxn<DateTime>>('birthday');
 }
--- ttos/lib/screen/main/tab/stock/setting/s_setting.dart
+++ ttos/lib/screen/main/tab/stock/setting/s_setting.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ttos/common/common.dart';
+import 'package:ttos/common/dart/extension/datetime_extension.dart';
 import 'package:ttos/common/data/preference/prefs.dart';
+import 'package:ttos/common/widget/w_big_button.dart';
 import 'package:ttos/screen/main/tab/stock/setting/w_switch_menu.dart';
 
 class SettingScreen extends StatefulWidget {
@@ -25,16 +27,28 @@ class _SettingScreenState extends State<SettingScreen> {
               onTap: (isOn) => Prefs.isPushOnRx.set(isOn),
             ),
           ),
-
           Obx(
             () => Slider(
               value: Prefs.sliderPosiion.get(),
               onChanged: (val) => Prefs.sliderPosiion(val),
             ),
-          )
+          ),
+          Obx(
+            () => BigButton(
+              '날짜 ${Prefs.birthday.get() == null ? '' : Prefs.birthday.get()?.formattedDate}',
+              onTap: () async {
+                final DateTime? date = await showDatePicker(
+                  context: context,
+                  firstDate: DateTime.now().subtract(90.days),
+                  lastDate: DateTime.now().add(90.days),
+                );
 
-          // date time
+                if (date == null) return;
 
+                Prefs.birthday(date);
+              },
+            ),
+          ),
           // number dialog
         ],
       ),
