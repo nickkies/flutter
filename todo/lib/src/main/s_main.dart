5c59c0f0c6ff4902c847737558046441bc9add39
YoonSung Kim <opqudk@gmail.com>
2024-12-29 16:34:12 +0900

    :sparkles: feat: Structure

---
+++ todo/lib/src/main/s_main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:todo/src/main/tab/search/f_search.dart';
+import 'package:todo/src/main/tab/todo/f_todo.dart';
+
+import '../settings/settings_view.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  static const routeName = '/';
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIdx = 0;
+
+  final List<Widget> _pages = [
+    const TodoFragment(),
+    const SearchFragment(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('TODO'), actions: [
+        IconButton(
+          icon: const Icon(Icons.settings),
+          onPressed: () {
+            Navigator.restorablePushNamed(context, SettingsView.routeName);
+          },
+        ),
+      ]),
+      body: _pages[_selectedIdx],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIdx,
+          onTap: (idx) {
+            setState(() {
+              _selectedIdx = idx;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.event_note_outlined),
+              label: 'TODO',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            )
+          ]),
+    );
+  }
+}
