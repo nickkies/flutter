54b3101e484a329e6661486bdf386e26cf54c429
YoonSung Kim <opqudk@gmail.com>
2025-01-07 16:29:22 +0900

    :pencil2: typo

--- todo/lib/src/data/memory/todo_data_notifier.dart
+++ todo/lib/src/data/memory/todo_data_notifier.dart
@@ -2,8 +2,8 @@ import 'package:flutter/cupertino.dart';
 
 import 'vo_todo.dart';
 
-class TodoDateNotifier extends ValueNotifier<List<Todo>> {
-  TodoDateNotifier() : super([]);
+class TodoDataNotifier extends ValueNotifier<List<Todo>> {
+  TodoDataNotifier() : super([]);
 
   void addTodo(Todo todo) {
     value.add(todo);
