a001819e9ca805d9c9b89c1a1aa9757954bca526
YoonSung Kim <opqudk@gmail.com>
2025-01-08 16:47:14 +0900

    :sparkles: feat: Add todo data holder

---
+++ todo/lib/src/data/memory/todo_data_holder.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todo/src/data/memory/todo_data_notifier.dart';
+
+class TodoDataHolder extends InheritedWidget {
+  final TodoDataNotifier notifier;
+
+  const TodoDataHolder({
+    super.key,
+    required super.child,
+    required this.notifier,
+  });
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return true;
+  }
+}
