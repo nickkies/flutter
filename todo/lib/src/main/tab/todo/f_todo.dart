dc5eee858f3f15a593617c104d25a5b62d4fbd09
YoonSung Kim <opqudk@gmail.com>
2025-01-10 11:18:40 +0900

    :sparkles: feat: Add empty todo list

--- todo/lib/src/main/tab/todo/f_todo.dart
+++ todo/lib/src/main/tab/todo/f_todo.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:todo/src/data/memory/todo_data_holder.dart';
 
 class TodoFragment extends StatelessWidget {
   const TodoFragment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('todo');
+    return ValueListenableBuilder(
+      valueListenable: TodoDataHolder.of(context).notifier,
+      builder: (context, todoList, child) => todoList.isEmpty
+          ? Center(
+              child: Text('할일을 작성해 주세요.',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  )))
+          : Column(children: todoList.map((e) => Text(e.title)).toList()),
+    );
   }
 }
