3910e78c3ebfe2b26da7aa5bd270d581c139e575
YoonSung Kim <opqudk@gmail.com>
2025-06-24 10:09:34 +0900

    ✨ feat: Add Category and Product models

---
+++ behboolun_meenjoke/lib/model/category.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'category.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_Category _$CategoryFromJson(Map<String, dynamic> json) =>
+    _Category(title: json['title'] as String?, docId: json['docId'] as String?);
+
+Map<String, dynamic> _$CategoryToJson(_Category instance) => <String, dynamic>{
+  'title': instance.title,
+  'docId': instance.docId,
+};
